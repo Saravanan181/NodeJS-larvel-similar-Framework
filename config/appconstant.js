37fd9c2766a7e1267b1cfdb9a0280344ad33a515
saravanan <saravanan.j@innoppl.com>
2020-05-27 13:23:19 +0530

    constant

--- config/appconstant.js
+++ config/appconstant.js
@@ -1,3 +1,4 @@
 module.exports = Object.freeze({
-    SECTIONLISTURL: 'https://dev-ethos.enterpriseapplicationdevelopers.com:6005/category'
+    SECTIONLISTURL: 'https://dev-ethos.enterpriseapplicationdevelopers.com:6005/category',
+    SECTIONOVERLLDOWNLOAD: 'https://dev-ethos.enterpriseapplicationdevelopers.com:6005/Keysiteinfo/get_all_documents/'
 });
